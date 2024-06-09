48f912c6df55c6c81f8aaec3fe6c0f15589f1a48
StellaTourino <stellatcpinto@gmail.com>
2024-06-09 18:19:59 -0300

    Create Data.cpp

---
+++ Data.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Data{
+	private:
+		int dia, mes, ano;
+	public:
+		int getDia(){
+			return dia;
+		}
+		void setDia(int dia){
+			this->dia = dia;
+		}
+		int getMes(){
+			return mes;
+		}
+		void setMes(int mes){
+			this->mes = mes;
+		}
+		int getAno(){
+			return ano;
+		}
+		void setAno (int ano){
+			this->ano = ano;
+		}
+};
